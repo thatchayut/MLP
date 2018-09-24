7f838ab5fe5e3b1b1641d6bfb05f85c1599a7323
thatchayut <thatchayut.u@gmail.com>
2018-09-24 14:24:58 +0700

    wrong iterations leading to wrong value assignment in the last position of arr_y are fixed

--- cross_validation.py
+++ cross_validation.py
@@ -136,7 +136,8 @@ def forward (dataframe_input, dataframe_output, data_all, line, arr_input_nodes,
         # CALCULATE Y of each node only when INPUT and OUTPUT are VALID
     if ((input_check == True) and (output_check == True)):
         print("BEFORE... output nodes : " + str(arr_output_nodes))
-        for layer_index in range(0, len(arr_Y)):
+        # iterations = len(arr_Y) + 1
+        for layer_index in range(0, len(arr_Y) + 1):
             # weight from an input layer to the 1st hidden layer
             if (layer_index == 0):
                 for index in range(0, len(arr_Y[layer_index])):
@@ -147,25 +148,26 @@ def forward (dataframe_input, dataframe_output, data_all, line, arr_input_nodes,
                     # modify output using activation function
                     arr_Y[layer_index][index] = useFunction(arr_Y[layer_index][index], function_number, beta)
             # calcualte output at the last hidden layer
-            elif (layer_index == (len(arr_Y) - 1)):
+            elif (layer_index == (len(arr_Y))):
                 # print("testtttttttttttttttttttttt")
                 for output_index in range(0, len(arr_output_nodes)):
-                    for index in range(0, len(arr_Y[layer_index])):
+                    for index in range(0, len(arr_Y[layer_index - 1])):
                         for node_index in range(0, len(arr_hidden_layers[2])):
                             for weight in arr_hidden_layers[2][node_index]:
-                                arr_output_nodes[output_index] += (weight * arr_Y[layer_index][index])
+                                arr_output_nodes[output_index] += (weight * arr_Y[layer_index - 1][index])
                     arr_output_nodes[output_index] += (arr_weight_bias_output[output_index] * arr_bias_output[output_index])
-                    arr_output_nodes[output_index] = useFunction(arr_output_nodes[output_index], function_number, beta) 
+                    # arr_output_nodes[output_index] = useFunction(arr_output_nodes[output_index], function_number, beta) 
             else:
             # calculate output for all nodes in hidden layers except the first layer connected to input node
-                for layer_index in range(1, len(arr_Y)):
-                    for index in range(0, len(arr_Y[layer_index])):
-                        for weight_layer in range(0, len(arr_hidden_layers[1])):
-                            for node_index in range(0, len(arr_hidden_layers[1][weight_layer])):
-                                for weigth_to_node in range(0, len(arr_hidden_layers[1][weight_layer][node_index])):
-                                    arr_Y[layer_index][index] += (arr_Y[layer_index - 1][node_index] * arr_hidden_layers[1][weight_layer][node_index][weigth_to_node])
-                                    arr_Y[layer_index][index] += (arr_weight_bias[layer_index][index] * arr_bias[layer_index][index])
-                    arr_Y[layer_index][index] = useFunction(arr_Y[layer_index][index], function_number, beta)
+                # for layer_index in range(1, len(arr_Y) - 2):
+                for index in range(0, len(arr_Y[layer_index])):
+                    for weight_layer in range(0, len(arr_hidden_layers[1])):
+                        for node_index in range(0, len(arr_hidden_layers[1][weight_layer])):
+                            for weigth_to_node in range(0, len(arr_hidden_layers[1][weight_layer][node_index])):
+                                # print("arr_Y[" + str(layer_index) + "][" + str(node_index) + "]")
+                                arr_Y[layer_index][index] += (arr_Y[layer_index - 1][node_index] * arr_hidden_layers[1][weight_layer][node_index][weigth_to_node])
+                                arr_Y[layer_index][index] += (arr_weight_bias[layer_index][index] * arr_bias[layer_index][index])
+                arr_Y[layer_index][index] = useFunction(arr_Y[layer_index][index], function_number, beta)
         print("arr_Y" + str(arr_Y))
         print("arr_output_nodes(actual output) : " + str(arr_output_nodes))
         print("data output(desired output)  : " + str(data_output))
@@ -186,16 +188,33 @@ def forward (dataframe_input, dataframe_output, data_all, line, arr_input_nodes,
     # print("arr_output_nodes after reset : " + str(arr_output_nodes))
     # print("------------------------------------------------------------------------------------------------------")
 
-def backward(arr_grad_hidden, arr_grad_output, arr_Y, arr_output_nodes, arr_error):
-    new_arr = []
-    new_arr.append(arr_Y)
-    new_arr.append(arr_output_nodes)
+def backward(arr_grad_hidden, arr_grad_output, arr_Y, arr_output_nodes, arr_error, function_number):
+    arr_output_merged = []
+    arr_output_merged.append(arr_Y)
+    arr_output_merged.append(arr_output_nodes)
     arr_grad = []
     arr_grad.append(arr_grad_hidden)
     arr_grad.append(arr_grad_output)
+    print("BEFORE.......")
     print("arr_Y : " + str(arr_Y))
-    print("new_arr" + str(new_arr))
+    print("arr_output_merged" + str(arr_output_merged))
     print("arr_grad_hidden, arr_grad_output" + str(arr_grad))
+    print("arr_error : " + str(arr_error))
+    # calculate local gradient
+    # iterate loop in common way but call element in reversed position
+    for list_index in range(0, len(arr_grad)):
+        # in case of output layer
+        if(list_index == 0):
+            # in case of using Sigmoid function
+            if(function_number == "1"):
+                for output_index in range(0, len(arr_output_nodes)):
+                    arr_grad[len(arr_grad) - list_index - 1] = arr_error[output_index] * arr_output_nodes[output_index] * \
+                                                               (1 - arr_output_nodes[output_index])
+    print("AFTER.......")
+    print("arr_Y : " + str(arr_Y))
+    print("arr_output_merged" + str(arr_output_merged))
+    print("arr_grad_hidden, arr_grad_output" + str(arr_grad))
+    print("arr_error : " + str(arr_error))
 
 def crossValidation(input_file, output_file, full_data_file, number_of_fold, arr_input_nodes, arr_hidden_layers, arr_Y, arr_output_nodes, arr_weight_bias, arr_bias, \
                     arr_weight_bias_output, arr_bias_output, function_number, momentum, learning_rate, beta, arr_grad_hidden, arr_grad_output):
@@ -223,11 +242,16 @@ def crossValidation(input_file, output_file, full_data_file, number_of_fold, arr
                 print(train_element)
                 print()
                 for element in train_element:
-
+                    print("*****************************************************************************************************")
+                    print("                                           FORWARD                                                   ")
+                    print("*****************************************************************************************************")
                     sse, arr_error = forward(dataframe_input, dataframe_output, data_all, element, arr_input_nodes, arr_output_nodes, arr_Y, \
                     arr_hidden_layers, arr_weight_bias, arr_bias, arr_weight_bias_output, arr_bias_output, function_number, beta)
 
-                    backward(arr_grad_hidden, arr_grad_output, arr_Y, arr_output_nodes, arr_error)
+                    print("*****************************************************************************************************")
+                    print("                                           BACKWARD                                                   ")
+                    print("*****************************************************************************************************")
+                    backward(arr_grad_hidden, arr_grad_output, arr_Y, arr_output_nodes, arr_error, function_number)
 
                     #reset arr_Y
                     for layer_index in range(0, len(arr_Y)):
